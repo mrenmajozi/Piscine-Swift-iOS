7f7a1792bbc85559f9a4f423b8e616a45948ee69
Njabulo MAJOZI <nmajozi@e4r13p3.wethinkcode.co.za>
2018-10-01 13:31:40 +0200

    Make display screen updating and debug function

--- d00/ex02/Moar buttons/Moar buttons/ViewController.swift
+++ d00/ex02/Moar buttons/Moar buttons/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayScreenLabel: UILabel!
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        displayScreenLabel.text = displayScreenLabel.text! + String(sender.tag)
+        print("Clicked: " + String(sender.tag))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
